df11f1f068603d9d45cb54d636c5584513128e9c
amirshokraie <amirshokraie@outlook.com>
2025-07-28 11:34:25 +0330

    test_initialization_errors

--- rps_test.py
+++ rps_test.py
@@ -9,6 +9,29 @@ class TestRPSComparison(unittest.TestCase):
         self.paper = Paper()
         self.scissors = Scissors()
 
+    def test_initialization_errors(self):
+
+        # Case 1 : _value is not set (is None) -> NotImplementedError:
+        class NoneValue(BaseMove):
+            pass
+
+        with self.assertRaises(NotImplementedError):
+            NoneValue()
+
+        # Case 2 : _value is not str -> TypeError
+        class NotStringValue(BaseMove):
+            _value = 1234
+        
+        with self.assertRaises(TypeError):
+            NotStringValue()
+
+        # case 3 : _value is a string but invalid -> ValueError
+        class InvalidStringValue(BaseMove):
+            _value = "Invalid text for test"
+
+        with self.assertRaises(ValueError):
+            InvalidStringValue()
+    
     def test_equal(self):
         self.assertEqual(self.rock, Rock())
         self.assertEqual(self.paper, Paper())
